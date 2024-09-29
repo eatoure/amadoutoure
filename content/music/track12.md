e149abec68064dc34bb4ac7802562a5237569e5c
eatoure <etoure33@gmail.com>
2024-09-29 04:05:13 -0500

    Made music page. Also added PDF to facilitate me making other pages

---
+++ content/music/track12.md
@@ -0,0 +1,9 @@
+---
+title: "Getting Better"
+artist: "Uncle Ams"
+album: "Melancholies"
+year: "2022"
+url: "/music/Prod62.mp3"
+---
+
+Same as above. This is around the time I started really, really enjoying beatmaking.
--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/music`,
+        name: `music`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
