c2e2a715acef74d7743033b40ba1104d47fdc73d
eatoure <61247837+eatoure@users.noreply.github.com>
2025-10-14 12:36:52 -0500

    updating github username

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/eamadoutoure',
+      url: 'https://github.com/eatoure',
     },
     {
       name: 'Instagram',
